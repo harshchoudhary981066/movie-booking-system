b4f7627541d8e5ef568455a7a849ce0c7b9334d4
Palak Khurana <palakkhurana06@gmail.com>
2024-07-04 10:38:32 +0530

    Interfaces made in repository

--- movie-backend/src/main/java/com/example/movie_booking/repository/CustomerRepository.java
+++ movie-backend/src/main/java/com/example/movie_booking/repository/CustomerRepository.java
@@ -1,4 +1,14 @@
 package com.example.movie_booking.repository;
 
-public interface CustomerRepository {
+import com.example.movie_booking.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+    Optional<Customer> findByCustomerId(String customerId);
+
 }
