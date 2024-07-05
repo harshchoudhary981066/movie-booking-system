9df97c0d680288f674db53b89ac587a7d6e488b6
harshchoudhary981066 <harsh981066@gmail.com>
2024-07-05 13:08:32 +0530

    Dependencies updated in pom.xml

--- movie-backend/src/main/java/com/example/movie_booking/service/CustomerService.java
+++ movie-backend/src/main/java/com/example/movie_booking/service/CustomerService.java
@@ -2,7 +2,7 @@ package com.example.movie_booking.service;
 
 import com.example.movie_booking.model.Theater;
 import com.example.movie_booking.model.Ticket;
-import model.*;
+import com.example.movie_booking.model.*;
 public interface CustomerService {
     boolean chooseCity(String city);
     boolean bookMovieTicket(Theater theater);
