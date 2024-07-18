2cde30899853efdbe8d80ba632e341073ce164bb
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-07-18 15:32:40 +0530

    Add files via upload

--- movie-frontend/movie-app/src/app/app.component.ts
+++ movie-frontend/movie-app/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SlotComponent } from './slot/slot.component';
+import { BookslotComponent } from "./bookslot/bookslot.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet, LoginComponent, DashboardComponent, SlotComponent, BookslotComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- movie-frontend/movie-app/src/app/app.module.ts
+++ movie-frontend/movie-app/src/app/app.module.ts
@@ -1,2 +1,36 @@
+import {NgModule}from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { BookingsComponent } from "./bookings/bookings.component";
+import { BookslotComponent } from "./bookslot/bookslot.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LoginComponent } from "./login/login.component";
+import { SlotComponent } from "./slot/slot.component";
+import { HttpClient } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NoopAnimationDriver } from "@angular/animations/browser";
+
+@NgModule({
+    declarations:[
+        // AppComponent,
+        // BookslotComponent,
+        // LoginComponent,
+        // SlotComponent,
+        // BookingsComponent,
+        // DashboardComponent
+    ],
+    imports:[
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+        // AppRoutingModule,
+        // HttpClientModule,
+
+        // NoopAnimationsModule,
 
 
+    // providers:[BookingsComponent],
+    // bootstrap:[AppComponent]
+})
+export class AppModule{}
