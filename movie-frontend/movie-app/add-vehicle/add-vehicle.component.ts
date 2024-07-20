4ecf39030990b2d0f98f89d0b2e20cedc453ffae
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-07-20 16:21:16 +0530

    Add files via upload

---
+++ movie-frontend/movie-app/add-vehicle/add-vehicle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-vehicle',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-vehicle.component.html',
+  styleUrl: './add-vehicle.component.css'
+})
+export class AddVehicleComponent implements onInit{
+  vehicle$
+}
